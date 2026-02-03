e1428b8cf113b9eabcca304cf8b7da57d7e4a1ec
AndreLuis933 <andreluissouza933@gmail.com>
2026-02-02 20:55:52 -0400

    .

--- recharge/src/login.py
+++ recharge/src/login.py
@@ -5,10 +5,17 @@ from http_utils import post
 from wait_redis_code import wait_for_code
 
 
+class CodigoNaoRecebidoError(Exception):
+    """Exceção lançada quando o código não é recebido dentro do tempo limite."""
+
+
 def fazer_login(session: requests.Session):
     post(session, SMS_URL, {"msisdn": MSISDN}, "Enviar sms")
 
     code = wait_for_code()
+    if code is None:
+        msg = "Codigo nao chegou dentro de 5 minutos"
+        raise CodigoNaoRecebidoError(msg)
 
     login_payload = {
         "channel": "VIVO_WEB",
